e5724523e291cf0b31e8cbe59c892b1d418cfe31
thomasindrias <thoin216@student.liu.se>
2020-09-29 13:14:55 +0200

    unsplash working
    Co-authored-by: Adam Morén <adamo472@student.liu.se>

--- GameState.js
+++ GameState.js
@@ -1,74 +1,90 @@
 class Player {
-    constructor(name) {
-        this.name = name;
-        this.img = null;
-        this.keyword = null;
-    }
+  constructor(name) {
+    this.name = name;
+    this.img = null;
+    this.keyword = null;
+  }
 }
 
 class GameState {
-    constructor(players) {
-        this.players = players;
-        this.playerIndex = 0;
-        this.state = 0;
-        this.stateID = ['draw-state', 'ai-state', 'guess-state', 'end-state'];
+  constructor(players) {
+    this.players = players;
+    this.playerIndex = 0;
+    this.state = 0;
+    this.stateID = ['draw-state', 'ai-state', 'guess-state', 'end-state'];
+  }
+
+  drawState() {
+    clearCanvas();
+    this.show('draw-state');
+
+
+  }
+
+  AIState() {
+    this.show('ai-state');
+
+    // Get prediction
+    var keyword = getFrame();
+
+    // If player draws so bad so AI cant predict, continue with a random word
+    if (keyword === null) {
+      const random = Math.floor(Math.random() * categories.length);
+      keyword = categories[random];
     }
 
-    drawState() {
-        this.show('draw-state');
 
+    const url = 'https://api.unsplash.com/search/photos';
 
-    }
+    $.ajax({
+      url: url,
+      method: 'GET',
+      data: {
+        query: keyword,
+        'per_page': 1
+      },
+      headers: {
+        "Authorization": 'Client-ID ' + APIKEY
+      }
+    }).done(data => {
+      console.log(data);
+      var image = document.getElementById('img-pred');
+      image.src = data.results[0].urls.small;
 
-    AIState() {
-        this.show('ai-state');
+    }).fail(err => {
+      throw err;
+    });
 
-        // Get prediction
-        var keyword = getFrame();
+  }
 
-        const url = 'https://api.unsplash.com/photos/random';
-        $.ajax({
-            url: url,
-            method: 'GET',
-            headers: {
-                client_id: APIKEY
-            }
-        }).done(data => {
-            var url = window.URL.createObjectURL(data);
-            $("#img-pred").src = url;
-        }).fail(err => {
-            throw err;
-        });
-    }
+  guessState() {
+    this.show('guess-state');
 
-    guessState() {
-        this.show('guess-state');
+  }
 
-    }
+  endState() {
+    this.show('end-state');
+  }
 
-    endState() {
-        this.show('end-state');
-    }
+  show(id) {
+    // Show the chosen ID
+    this.stateID.forEach(state => {
+      if (id === state)
+        document.getElementById(state).style.setProperty("display", "block", "important");
+      else
+        document.getElementById(state).style.setProperty("display", "none", "important");
+    });
+  }
 
-    show(id) {
-        // Show the chosen ID
-        this.stateID.forEach(state => {
-            if (id === state)
-                document.getElementById(state).style.setProperty("display", "block", "important");
-            else
-                document.getElementById(state).style.setProperty("display", "none", "important");
-        });
-    }
+  restart() {
+    console.log("restart");
 
-    restart() {
-        console.log("restart");
+    document.getElementById('welcome-state').style.setProperty("display", "block", "important");
+    document.getElementById('game-state').style.setProperty("display", "none", "important");
+    $('#players').empty();
 
-        document.getElementById('welcome-state').style.setProperty("display", "block", "important");
-        document.getElementById('game-state').style.setProperty("display", "none", "important");
-        $('#players').empty();
-
-        this.players = [];
-        this.playerIndex = 0;
-        this.state = 0;
-    }
+    this.players = [];
+    this.playerIndex = 0;
+    this.state = 0;
+  }
 }
--- main.js
+++ main.js
@@ -11,299 +11,300 @@ var players = [];
 const APIKEY = 'y-bm_k6E6SHHkWoUXCcgPDrHRp-YhwbtwHRASagi9mM';
 
 $(() => {
-    init();
-    canvas = new fabric.Canvas('canvas');
-    canvas.backgroundColor = '#ffffff';
-    canvas.isDrawingMode = 0;
-    canvas.freeDrawingBrush.color = "black";
-    canvas.freeDrawingBrush.width = 10;
-    canvas.renderAll();
+  init();
+  canvas = new fabric.Canvas('canvas');
+  canvas.backgroundColor = '#ffffff';
+  canvas.isDrawingMode = 0;
+  canvas.freeDrawingBrush.color = "black";
+  canvas.freeDrawingBrush.width = 10;
+  canvas.renderAll();
 
-    //listeners
-    canvas.on('mouse:up', function (e) {
-        //getFrame();
-        mousePressed = false;
-    });
+  //listeners
+  canvas.on('mouse:up', function (e) {
+    //getFrame();
+    mousePressed = false;
+  });
 
-    canvas.on('mouse:down', function (e) {
-        mousePressed = true;
+  canvas.on('mouse:down', function (e) {
+    mousePressed = true;
 
-    });
+  });
 
-    canvas.on('mouse:move', function (e) {
-        recordCoords(e);
-    });
+  canvas.on('mouse:move', function (e) {
+    recordCoords(e);
+  });
 
 });
 
 async function init() {
-    // Load model
-    model = await tf.loadLayersModel(path + '/model.json');
+  // Load model
+  model = await tf.loadLayersModel(path + '/model.json');
 
-    // Test
-    model.predict(tf.zeros([1, imgSize, imgSize, 1]));
+  // Test
+  model.predict(tf.zeros([1, imgSize, imgSize, 1]));
 
-    //allow draw
-    canvas.isDrawingMode = 1;
+  //allow draw
+  canvas.isDrawingMode = 1;
 
-    await loadCategories();
+  await loadCategories();
 }
 
 // Record the drawings coords
 function recordCoords(event) {
-    var pointer = canvas.getPointer(event.e);
-    var x = pointer.x;
-    var y = pointer.y;
+  var pointer = canvas.getPointer(event.e);
+  var x = pointer.x;
+  var y = pointer.y;
 
-    if (x >= 0 && y >= 0 && mousePressed) {
-        coords.push(pointer);
-    }
+  if (x >= 0 && y >= 0 && mousePressed) {
+    coords.push(pointer);
+  }
 }
 
 function getFrame() {
-    if (coords.length >= 2) {
+  if (coords.length >= 2) {
 
-        const imgData = getImageData();
+    const imgData = getImageData();
 
-        const pred = model.predict(preprocess(imgData)).dataSync();
+    const pred = model.predict(preprocess(imgData)).dataSync();
 
-        //find prediction
-        const indices = findIndicesOfMax(pred, 5);
-        const probs = findTopValues(pred, 5);
-        const names = getCategories(indices);
+    //find prediction
+    const indices = findIndicesOfMax(pred, 5);
+    const probs = findTopValues(pred, 5);
+    const names = getCategories(indices);
 
-        console.log(indices);
-        console.log(probs);
-        console.log(names);
+    console.log(indices);
+    console.log(probs);
+    console.log(names);
 
-        $('#pred').text(names[0].split('_').join(' ') + ' ' + probs[0].toFixed(3) * 100 + '% probability');
-        return names[0].split('_').join(' ')
-    }
+    return names[0].split('_').join(' ');
+  }
 }
 
 // get categories
 function getCategories(indices) {
-    var res = [];
-    for (let i = 0; i < indices.length; i++) {
-        res[i] = categories[indices[i]];
-    }
+  var res = [];
+  for (let i = 0; i < indices.length; i++) {
+    res[i] = categories[indices[i]];
+  }
 
-    return res;
+  return res;
 }
 
 async function loadCategories() {
-    txt_path = path + '/class_names.txt';
+  txt_path = path + '/class_names.txt';
 
-    await $.ajax({
-        url: txt_path,
-        dataType: 'text'
-    }).done((data) => {
-        //If read successfully
-        const categoriesList = data.split("\n");
-        for (let i = 0; i < categoriesList.length; i++) {
-            let category = categoriesList[i];
-            categories[i] = category;
-        }
-    });
+  await $.ajax({
+    url: txt_path,
+    dataType: 'text'
+  }).done((data) => {
+    //If read successfully
+    const categoriesList = data.split("\n");
+    for (let i = 0; i < categoriesList.length; i++) {
+      let category = categoriesList[i];
+      categories[i] = category;
+    }
+  });
 }
 
 function findIndicesOfMax(pred, maxPred) {
-    var res = [];
-    for (let i = 0; i < pred.length; i++) {
-        res.push(i);
-        if (res.length > maxPred) {
-            res.sort((a, b) => {
-                return pred[b] - pred[a];
-            }); // Sort in descending order;
-            res.pop();
-        }
-
+  var res = [];
+  for (let i = 0; i < pred.length; i++) {
+    res.push(i);
+    if (res.length > maxPred) {
+      res.sort((a, b) => {
+        return pred[b] - pred[a];
+      }); // Sort in descending order;
+      res.pop();
     }
 
-    return res;
+  }
+
+  return res;
 }
 
 function findTopValues(pred, maxPred) {
-    var res = [];
-    let indices = findIndicesOfMax(pred, maxPred);
+  var res = [];
+  let indices = findIndicesOfMax(pred, maxPred);
 
-    // Get top 5 preds
-    for (let i = 0; i < indices.length; i++) {
-        res[i] = pred[indices[i]];
-    }
+  // Get top 5 preds
+  for (let i = 0; i < indices.length; i++) {
+    res[i] = pred[indices[i]];
+  }
 
-    return res;
+  return res;
 }
 
 //get the best bounding box by trimming around the drawing
 function getMinBoundingBox() {
-    //get x-coordinates 
-    var xCoor = coords.map((c) => {
-        return c.x;
-    });
+  //get x-coordinates 
+  var xCoor = coords.map((c) => {
+    return c.x;
+  });
 
-    //get y-coordinates
-    var yCoor = coords.map((c) => {
-        return c.y;
-    });
+  //get y-coordinates
+  var yCoor = coords.map((c) => {
+    return c.y;
+  });
 
-    //find min x and y coordinates
-    var min_coords = {
-        x: Math.min.apply(null, xCoor),
-        y: Math.min.apply(null, yCoor)
-    };
+  //find min x and y coordinates
+  var min_coords = {
+    x: Math.min.apply(null, xCoor),
+    y: Math.min.apply(null, yCoor)
+  };
 
-    //find max x and y coordinates
-    var max_coords = {
-        x: Math.max.apply(null, xCoor),
-        y: Math.max.apply(null, yCoor)
-    };
+  //find max x and y coordinates
+  var max_coords = {
+    x: Math.max.apply(null, xCoor),
+    y: Math.max.apply(null, yCoor)
+  };
 
-    return {
-        min: min_coords,
-        max: max_coords
-    };
+  return {
+    min: min_coords,
+    max: max_coords
+  };
 }
 
 //get the image data
 function getImageData() {
-    //get the minimum bounding box around the drawing
-    const mbb = getMinBoundingBox();
+  //get the minimum bounding box around the drawing
+  const mbb = getMinBoundingBox();
 
-    //get image data according to dpi (the ratio of the resolution for current device)
-    const dpi = window.devicePixelRatio;
+  //get image data according to dpi (the ratio of the resolution for current device)
+  const dpi = window.devicePixelRatio;
 
-    // (x: x-axis coordinate of the top-left corner, y: y-axis coordinate of the top-left corner, z: width, w: height)
-    const imgData = canvas.contextContainer.getImageData(mbb.min.x * dpi, mbb.min.y * dpi, (mbb.max.x - mbb.min.x) * dpi, (mbb.max.y - mbb.min.y) * dpi);
+  // (x: x-axis coordinate of the top-left corner, y: y-axis coordinate of the top-left corner, z: width, w: height)
+  const imgData = canvas.contextContainer.getImageData(mbb.min.x * dpi, mbb.min.y * dpi, (mbb.max.x - mbb.min.x) * dpi, (mbb.max.y - mbb.min.y) * dpi);
 
-    return imgData;
+  return imgData;
 }
 
 function preprocess(imgData) {
-    return tf.tidy(() => {
-        // Convert to tensor
-        let tensor = tf.browser.fromPixels(imgData, nrChannels = 1);
+  return tf.tidy(() => {
+    // Convert to tensor
+    let tensor = tf.browser.fromPixels(imgData, nrChannels = 1);
 
-        // Resize
-        const resize = tf.image.resizeBilinear(tensor, [imgSize, imgSize]).toFloat();
-
-
-        // Normalize => 1.0 - (data/255.0)
-        const offset = tf.scalar(255.0);
-        const normalize = tf.scalar(1.0).sub(resize.div(offset));
+    // Resize
+    const resize = tf.image.resizeBilinear(tensor, [imgSize, imgSize]).toFloat();
 
 
-        // Batch Shape
-        const batch = normalize.expandDims(0);
+    // Normalize => 1.0 - (data/255.0)
+    const offset = tf.scalar(255.0);
+    const normalize = tf.scalar(1.0).sub(resize.div(offset));
 
 
-        return batch;
-    });
+    // Batch Shape
+    const batch = normalize.expandDims(0);
+
+
+    return batch;
+  });
 }
 
 //Clear the canvas
 function clearCanvas() {
-    canvas.clear();
-    canvas.backgroundColor = '#ffffff';
-    coords = [];
-    console.log('cleared');
+  canvas.clear();
+  canvas.backgroundColor = '#ffffff';
+  coords = [];
+  console.log('cleared');
 }
 
 //add player to list and array
 function addPlayer(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    // Getting box and ul by selecting id; 
-    let input = document.getElementById("box");
-    // let player = document.getElementById("players");
-    if (input.value != "") {
+  // Getting box and ul by selecting id; 
+  let input = document.getElementById("box");
+  // let player = document.getElementById("players");
+  if (input.value != "") {
 
-        const newPlayer = new Player(input.value);
-        players.push(newPlayer);
-        // console.log(players);
+    const newPlayer = new Player(input.value);
+    players.push(newPlayer);
+    // console.log(players);
 
-        if (players.length > 2) {
-            // console.log("more than 2 players")
-            $("#start-button").removeClass("disabled");
-            $("#start-button").addClass("enabled");
-            document.getElementById("start-button").disabled = false;
-        }
-
-        // Creating element and adding value to it
-        $('#players').prepend('<li class="list-group-item d-flex justify-content-between align-items-center">' + input.value + '<span class="badge badge-danger badge-pill">X</span></li>');
-        // let make_li = document.createElement("LI");
-        // make_li.appendChild(document.createTextNode(input.value));
-
-        // // Adding li to ul 
-        // player.appendChild(make_li);
-
-        // Reset the value of box 
-        input.value = "";
-
-        // //Delete a player on click    
-        // deleteBadge.onclick = function () {
-        //     this.parentNode.removeChild(this);
-        // };
-
+    if (players.length > 2) {
+      // console.log("more than 2 players")
+      $("#start-button").removeClass("disabled");
+      $("#start-button").addClass("enabled");
+      document.getElementById("start-button").disabled = false;
     }
+
+    // Creating element and adding value to it
+    $('#players').prepend('<li class="list-group-item d-flex justify-content-between align-items-center">' + input.value + '<span class="badge badge-danger badge-pill">X</span></li>');
+    // let make_li = document.createElement("LI");
+    // make_li.appendChild(document.createTextNode(input.value));
+
+    // // Adding li to ul 
+    // player.appendChild(make_li);
+
+    // Reset the value of box 
+    input.value = "";
+
+    // //Delete a player on click    
+    // deleteBadge.onclick = function () {
+    //     this.parentNode.removeChild(this);
+    // };
+
+  }
 }
 
-function stateHandler(restart = false) {
+function stateHandler(e = null, restart = false) {
+  if (e !== null)
+    e.preventDefault();
 
-    if (!gm) // Start our game
-        gm = new GameState([]);
+  if (!gm) // Start our game
+    gm = new GameState([]);
 
-    // Open game state
-    if (gm.playerIndex === 0) {
-        document.getElementById('welcome-state').style.setProperty("display", "none", "important");
-        document.getElementById('game-state').style.setProperty("display", "block", "important");
+  // Open game state
+  if (gm.playerIndex === 0) {
+    document.getElementById('welcome-state').style.setProperty("display", "none", "important");
+    document.getElementById('game-state').style.setProperty("display", "block", "important");
 
-        // Add players to game state
-        gm.players = players;
-    }
+    // Add players to game state
+    gm.players = players;
+  }
 
-    if (gm.players.length < 2) return;
+  if (gm.players.length < 2) return;
 
-    if (restart) {
-        console.log("Restart Game");
+  if (restart) {
+    console.log("Restart Game");
 
-        gm.restart();
-    }
+    gm.restart();
+  }
 
-    // End state
-    if (gm.playerIndex === gm.players.length) {
-        console.log("End state");
-        players = [];
-        gm.endState();
-        return;
-    }
+  // End state
+  if (gm.playerIndex === gm.players.length) {
+    console.log("End state");
+    players = [];
+    gm.endState();
+    return;
+  }
 
-    // Draw state
-    if (gm.state === 0) {
-        console.log("Draw State", gm.players[gm.playerIndex]);
+  // Draw state
+  if (gm.state === 0) {
+    console.log("Draw State", gm.players[gm.playerIndex]);
 
-        gm.drawState();
+    gm.drawState();
 
-        gm.playerIndex++;
-    }
+    gm.playerIndex++;
+  }
 
-    // AI state
-    else if (gm.state === 1) {
-        console.log("AI State");
+  // AI state
+  else if (gm.state === 1) {
+    console.log("AI State");
 
-        gm.AIState();
-    }
+    gm.AIState();
+  }
 
-    // Guess state
-    else if (gm.state === 2) {
-        console.log("Guess State", gm.players[gm.playerIndex]);
+  // Guess state
+  else if (gm.state === 2) {
+    console.log("Guess State", gm.players[gm.playerIndex]);
 
-        gm.guessState();
+    gm.guessState();
 
-        gm.playerIndex++;
-    }
+    gm.playerIndex++;
+  }
 
 
-    gm.state++;
-    gm.state = gm.state % 3;
+  gm.state++;
+  gm.state = gm.state % 3;
 }
