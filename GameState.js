c5d909afabc3700da9a937e984c463a7081c71d3
morenadam <adam.moren1@gmail.com>
2020-09-29 11:48:43 +0200

    wip
    Co-authored-by: Thomas Indrias Biniam <thoin216@student.liu.se>

--- GameState.js
+++ GameState.js
@@ -1,60 +1,74 @@
 class Player {
-  constructor(name) {
-    this.name = name;
-    this.img = null;
-    this.keyword = null;
-  }
+    constructor(name) {
+        this.name = name;
+        this.img = null;
+        this.keyword = null;
+    }
 }
 
 class GameState {
-  constructor(players) {
-    this.players = players;
-    this.playerIndex = 0;
-    this.state = 0;
-    this.stateID = ['draw-state', 'ai-state', 'guess-state', 'end-state'];
-  }
+    constructor(players) {
+        this.players = players;
+        this.playerIndex = 0;
+        this.state = 0;
+        this.stateID = ['draw-state', 'ai-state', 'guess-state', 'end-state'];
+    }
 
-  drawState() {
-    this.show('draw-state');
+    drawState() {
+        this.show('draw-state');
 
 
-  }
+    }
 
-  AIState() {
-    this.show('ai-state');
+    AIState() {
+        this.show('ai-state');
 
-    // Get prediction
-    getFrame();
-  }
+        // Get prediction
+        var keyword = getFrame();
 
-  guessState() {
-    this.show('guess-state');
+        const url = 'https://api.unsplash.com/photos/random';
+        $.ajax({
+            url: url,
+            method: 'GET',
+            headers: {
+                client_id: APIKEY
+            }
+        }).done(data => {
+            var url = window.URL.createObjectURL(data);
+            $("#img-pred").src = url;
+        }).fail(err => {
+            throw err;
+        });
+    }
 
-  }
+    guessState() {
+        this.show('guess-state');
 
-  endState() {
-    this.show('end-state');
-  }
+    }
 
-  show(id) {
-    // Show the chosen ID
-    this.stateID.forEach(state => {
-      if (id === state)
-        document.getElementById(state).style.setProperty("display", "block", "important");
-      else
-        document.getElementById(state).style.setProperty("display", "none", "important");
-    });
-  }
+    endState() {
+        this.show('end-state');
+    }
 
-  restart() {
-    console.log("restart");
+    show(id) {
+        // Show the chosen ID
+        this.stateID.forEach(state => {
+            if (id === state)
+                document.getElementById(state).style.setProperty("display", "block", "important");
+            else
+                document.getElementById(state).style.setProperty("display", "none", "important");
+        });
+    }
 
-    document.getElementById('welcome-state').style.setProperty("display", "block", "important");
-    document.getElementById('game-state').style.setProperty("display", "none", "important");
-    $('#players').empty();
+    restart() {
+        console.log("restart");
 
-    this.players = [];
-    this.playerIndex = 0;
-    this.state = 0;
-  }
+        document.getElementById('welcome-state').style.setProperty("display", "block", "important");
+        document.getElementById('game-state').style.setProperty("display", "none", "important");
+        $('#players').empty();
+
+        this.players = [];
+        this.playerIndex = 0;
+        this.state = 0;
+    }
 }
